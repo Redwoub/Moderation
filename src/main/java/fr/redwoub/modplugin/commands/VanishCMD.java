3b746ee2175f5351fc7b60effec037441f85f647
Redwoub <eloiverinaudcoma@gmail.com>
2021-02-06 20:19:17 +0100

    [fix] Player show on deconnect / reconnect

--- src/main/java/fr/redwoub/modplugin/commands/VanishCMD.java
+++ src/main/java/fr/redwoub/modplugin/commands/VanishCMD.java
@@ -1,12 +1,18 @@
 package fr.redwoub.modplugin.commands;
 
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VanishCMD implements CommandExecutor {
+
+    public static List<Player> playerVanish = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -25,15 +31,31 @@ public class VanishCMD implements CommandExecutor {
         Player player = (Player) sender;
 
         if(args[0].equals("on")){
-            player.hidePlayer(player);
+            playerVanish.add(player);
+            for(Player players : Bukkit.getOnlinePlayers()){
+                if(!players.hasPermission("mod.use")){
+                    players.hidePlayer(player);
+                }
+            }
             player.sendMessage("§aVanish on !");
         }
 
         if(args[0].equals("off")){
-            player.showPlayer(player);
+            for(Player players : Bukkit.getOnlinePlayers()){
+                players.showPlayer(player);
+            }
+            playerVanish.remove(player);
             player.sendMessage("§cVanish off !");
         }
 
         return false;
+    }
+
+    public static Player getPlayer(List<Player> player){
+
+        for(Player players : player){
+            return players;
+        }
+        return null;
     }
 }
--- src/main/java/fr/redwoub/modplugin/managers/PlayerManager.java
+++ src/main/java/fr/redwoub/modplugin/managers/PlayerManager.java
@@ -1,9 +1,12 @@
 package fr.redwoub.modplugin.managers;
 
+import fr.redwoub.modplugin.commands.VanishCMD;
 import fr.redwoub.modplugin.utils.Freeze;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerManager implements Listener {
@@ -15,6 +18,16 @@ public class PlayerManager implements Listener {
         if (Freeze.isFreeze(player) && event.getFrom().distance(event.getTo()) > 0) {
             player.teleport(player.getLocation());
             player.sendMessage("§cYou were freeze !\n §c If you log out you will be banned for life !");
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        if(!player.hasPermission("mod.use")){
+            for(Player players : Bukkit.getOnlinePlayers()){
+                players.hidePlayer(VanishCMD.getPlayer(VanishCMD.playerVanish));
+            }
         }
     }
 }
