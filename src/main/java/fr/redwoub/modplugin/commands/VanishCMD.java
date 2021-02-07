95db4321308f69ebc32eaa12593184fc60bd5388
Redwoub <eloiverinaudcoma@gmail.com>
2021-02-07 15:49:30 +0100

    [add] command vanish status

--- src/main/java/fr/redwoub/modplugin/commands/VanishCMD.java
+++ src/main/java/fr/redwoub/modplugin/commands/VanishCMD.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class VanishCMD implements CommandExecutor {
 
-    public static List<Player> playerVanish = new ArrayList<>();
+    public List<Player> playerVanish = new ArrayList<>();
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -24,13 +24,13 @@ public class VanishCMD implements CommandExecutor {
 
         if(args.length != 1){
             sender.sendMessage("§cInvalid syntax");
-            sender.sendMessage("§e/vanish on | off");
+            sender.sendMessage("§e/vanish on | off | status");
             return false;
         }
 
         Player player = (Player) sender;
 
-        if(args[0].equals("on")){
+        if(args[0].equalsIgnoreCase("on")){
             playerVanish.add(player);
             for(Player players : Bukkit.getOnlinePlayers()){
                 if(!players.hasPermission("mod.use")){
@@ -40,7 +40,7 @@ public class VanishCMD implements CommandExecutor {
             player.sendMessage("§aVanish on !");
         }
 
-        if(args[0].equals("off")){
+        if(args[0].equalsIgnoreCase("off")){
             for(Player players : Bukkit.getOnlinePlayers()){
                 players.showPlayer(player);
             }
@@ -48,11 +48,19 @@ public class VanishCMD implements CommandExecutor {
             player.sendMessage("§cVanish off !");
         }
 
+        if(args[0].equalsIgnoreCase("status")){
+            if(playerVanish.contains(player)){
+                player.sendMessage("§6Vanish on");
+            } else {
+                player.sendMessage("§6Vanish off");
+            }
+        }
+
         return false;
     }
 
-    public static Player getPlayer(List<Player> player){
-
+    public Player getPlayer(List<Player> player){
+        if(player.isEmpty()) return null;
         for(Player players : player){
             return players;
         }
